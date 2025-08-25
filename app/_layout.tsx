35c3a490ad596c426924018ead8f7a4be63978bd
Estéfano Tuyama <egerassi@gmail.com>
2025-08-25 13:33:02 -0300

    Add navigation setup

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,46 @@
-import { Stack } from "expo-router";
+import { Tabs } from 'expo-router';
+import FontAwesome from '@expo/vector-icons/FontAwesome';
 
-export default function RootLayout() {
-  return <Stack />;
+function TabBarIcon(props: { name: React.ComponentProps<typeof FontAwesome>['name']; color: string }) {
+    return <FontAwesome size={28} style={{ marginBottom: -3 }} {...props} />;
+}
+
+
+export default function TabLayout() {
+    return (
+        <Tabs>
+            <Tabs.Screen
+                name="index"
+                options={{
+                    title: 'Home',
+                    headerShown: false,
+                    tabBarIcon: ({ color }) => <TabBarIcon name="home" color={color} />,
+                }}
+            />
+            <Tabs.Screen
+                name="modalities"
+                options={{
+                    title: 'Modalidades',
+                    headerShown: false,
+                    tabBarIcon: ({ color }) => <TabBarIcon name="list" color={color} />,
+                }}
+            />
+            <Tabs.Screen
+                name="accomodations"
+                options={{
+                    title: 'Alojamentos',
+                    headerShown: false,
+                    tabBarIcon: ({ color }) => <TabBarIcon name="building" color={color} />,
+                }}
+            />
+            <Tabs.Screen
+                name="favorites"
+                options={{
+                    title: 'Meus Interesses',
+                    headerShown: false,
+                    tabBarIcon: ({ color }) => <TabBarIcon name="heart" color={color} />,
+                }}
+            />
+        </Tabs>
+    );
 }
--- app/favorites.tsx
+++ app/favorites.tsx
@@ -1,9 +1,9 @@
 import {Text, View} from "react-native";
 
-export default function Index() {
+export default function Favorites() {
     return (
         <View>
-            <Text>Home Screen</Text>
+            <Text>Meus Interesses</Text>
         </View>
     )
 }
