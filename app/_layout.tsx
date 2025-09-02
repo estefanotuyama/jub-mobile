7c2f6c87adea14b7b610b689495a78556c7fc0e7
estefanotuyama <egerassi@gmail.com>
2025-09-02 13:34:41 -0300

    add accomodation button and fix navigator

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,59 +2,60 @@ import { Tabs } from 'expo-router';
 import FontAwesome from '@expo/vector-icons/FontAwesome';
 
 function TabBarIcon(props: { name: React.ComponentProps<typeof FontAwesome>['name']; color: string }) {
-    return <FontAwesome size={28} style={{ marginBottom: -3 }} {...props} />;
+	return <FontAwesome size={28} style={{ marginBottom: -3 }} {...props} />;
 }
 
 
 export default function TabLayout() {
-    return (
-        <Tabs backBehavior="history">
-            <Tabs.Screen
-                name="index"
-                options={{
-                    title: 'Home',
-                    headerShown: false,
-                    tabBarIcon: ({ color }) => <TabBarIcon name="home" color={color} />,
-                }}
-            />
-            <Tabs.Screen
-                name="modalities/index"
-                options={{
-                    title: 'Modalidades',
-                    headerShown: false,
-                    tabBarIcon: ({ color }) => <TabBarIcon name="list" color={color} />,
-                }}
-            />
-            <Tabs.Screen
-                name="accomodations/index"
-                options={{
-                    title: 'Alojamentos',
-                    headerShown: false,
-                    tabBarIcon: ({ color }) => <TabBarIcon name="building" color={color} />,
-                }}
-            />
-            <Tabs.Screen
-                name="favorites/index"
-                options={{
-                    title: 'Meus Interesses',
-                    headerShown: false,
-                    tabBarIcon: ({ color }) => <TabBarIcon name="heart" color={color} />,
-                }}
-            />
-            <Tabs.Screen
-                name="modalities/[id]"
-                options={{
-                    href: null,
-                    headerShown: false
-                }}
-            />
-            <Tabs.Screen
-               name="accomodations/[id]"
-                options={{
-                    href: null,
-                    headerShown: false,
-                }}
-            />
-        </Tabs>
-    );
+	return (
+		<Tabs backBehavior="history">
+			<Tabs.Screen
+				name="index"
+				options={{
+					title: 'Home',
+					headerShown: false,
+					tabBarIcon: ({ color }) => <TabBarIcon name="home" color={color} />,
+				}}
+			/>
+			<Tabs.Screen
+				name="modalities/index"
+				options={{
+					title: 'Modalidades',
+					headerShown: false,
+					tabBarIcon: ({ color }) => <TabBarIcon name="list" color={color} />,
+				}}
+			/>
+			<Tabs.Screen
+				name="accomodations/index"
+				options={{
+					title: 'Alojamentos',
+					headerShown: false,
+					tabBarIcon: ({ color }) => <TabBarIcon name="building" color={color} />,
+				}}
+			/>
+			<Tabs.Screen
+				name="accomodations/[id]"
+				options={{
+					href: null,
+					headerShown: false,
+				}}
+			/>
+
+			<Tabs.Screen
+				name="favorites/index"
+				options={{
+					title: 'Meus Interesses',
+					headerShown: false,
+					tabBarIcon: ({ color }) => <TabBarIcon name="heart" color={color} />,
+				}}
+			/>
+			<Tabs.Screen
+				name="modalities/[id]"
+				options={{
+					href: null,
+					headerShown: false
+				}}
+			/>
+		</Tabs>
+	);
 }
