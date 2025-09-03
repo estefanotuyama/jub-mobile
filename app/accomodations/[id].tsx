1e359e7de922cba442c28fd177cd93cf3cedc6ed
Gean Pereira <gean.pereira@bridge.ufsc.br>
2025-09-03 00:38:48 -0300

    add carousel in accomodations

--- app/accomodations/[id].tsx
+++ app/accomodations/[id].tsx
@@ -1,142 +1,170 @@
-import { SafeAreaView, StatusBar, Text, View, StyleSheet, Pressable, Image, ScrollView, Platform, Linking } from "react-native";
-import { router, useLocalSearchParams } from "expo-router";
 import FontAwesome from "@expo/vector-icons/FontAwesome";
+import { router, useLocalSearchParams } from "expo-router";
+import { useEffect, useRef, useState } from "react";
+import {
+  Dimensions, Image, Linking, Platform, Pressable, SafeAreaView, ScrollView, StatusBar, StyleSheet, Text, View
+} from "react-native";
+
+
+const {width: screenWidth} = Dimensions.get('window');
 
 const mapUrl = Platform.select({
-    ios: 'maps:0,0?q=',
-    android: 'geo:0,0?q=',
+  ios: 'maps:0,0?q=', android: 'geo:0,0?q=',
 });
 
 export default function AccomodationDetails() {
-    const acommodation = useLocalSearchParams();
+  const acommodation = useLocalSearchParams();
 
-    return (
-        <SafeAreaView style={styles.container}>
-            <StatusBar barStyle={"default"} />
-            <ScrollView>
-                <View style={styles.contentContainer}>
-                    <Pressable style={styles.backButton} onPress={() => router.back()}>
-                        <Image source={require('../../assets/images/back-arrow.png')} style={styles.icon} />
-                    </Pressable>
+  const fotosArray = JSON.parse(acommodation.fotos as string || '[]');
+  const [activeIndex, setActiveIndex] = useState(0);
+  const scrollViewRef = useRef(null);
 
-                    <Text style={styles.title}>{acommodation.nome}</Text>
+  const handleScroll = (event) => {
+    const scrollPosition = event.nativeEvent.contentOffset.x;
+    const index = Math.round(scrollPosition / screenWidth);
+    setActiveIndex(index);
+  };
 
-                    <Image source={{ uri: acommodation.foto as string }} style={styles.image} />
+  const scrollToBeginning = () => {
+    if (scrollViewRef.current) {
+      scrollViewRef.current.scrollTo({x: 0, animated: false});
+      setActiveIndex(0);
+    }
+  };
 
-                    <View style={styles.infoCard}>
-                        <View style={styles.infoRow}>
-                            <FontAwesome name="money" size={20} color="#555" style={styles.infoIcon} />
-                            <View style={styles.infoTextContainer}>
-                                <Text style={styles.infoLabel}>Valor</Text>
-                                <Text style={styles.infoValue}>{acommodation.valor}</Text>
-                            </View>
-                        </View>
-                        <View style={styles.infoRow}>
-                            <FontAwesome name="envelope-o" size={20} color="#555" style={styles.infoIcon} />
-                            <View style={styles.infoTextContainer}>
-                                <Text style={styles.infoLabel}>Email para contato</Text>
-                                <Text style={styles.infoValue}>{acommodation.email}</Text>
-                            </View>
-                        </View>
+  useEffect(() => {
+    scrollToBeginning();
+  }, [acommodation.id]);
 
-                        <View style={styles.infoRow}>
-                            <FontAwesome name="phone" size={20} color="#555" style={styles.infoIcon} />
-                            <View style={styles.infoTextContainer}>
-                                <Text style={styles.infoLabel}>Telefone para contato</Text>
-                                <Text style={styles.infoValue}>{acommodation.telefone}</Text>
-                            </View>
-                        </View>
-                        <View style={styles.infoRow}>
-                            <FontAwesome name="map-marker" size={20} color="#555" style={styles.infoIcon} />
-                            <View style={styles.infoTextContainer}>
-                                <Text style={styles.infoLabel}>Localização</Text>
-                                <Text style={styles.infoValue}>{acommodation.endereco}</Text>
-                            </View>
-                        </View>
-                    </View>
+  return (<SafeAreaView style={styles.container}>
+    <StatusBar barStyle={"default"}/>
+    <ScrollView>
+      <View>
+        <ScrollView
+          horizontal
+          pagingEnabled
+          showsHorizontalScrollIndicator={false}
+          onScroll={handleScroll}
+          scrollEventThrottle={16}
+          ref={scrollViewRef}
+          onLayout={scrollToBeginning}
+          style={styles.carouselContainer}
+        >
+          {fotosArray.map((fotoUrl, index) => (<Image
+            key={index}
+            source={{uri: fotoUrl}}
+            style={styles.carouselImage}
+          />))}
+        </ScrollView>
+        <View style={styles.pagination}>
+          {fotosArray.map((_, index) => (<View
+            key={index}
+            style={[styles.paginationDot, index === activeIndex ? styles.paginationDotActive : {}]}
+          />))}
+        </View>
+      </View>
+      <Pressable style={styles.backButton} onPress={() => router.back()}>
+        <Image source={require('../../assets/images/back-arrow.png')} style={styles.icon}/>
+      </Pressable>
+      <View style={styles.contentContainer}>
+        <Text style={styles.title}>{acommodation.nome}</Text>
 
-                    <Pressable style={styles.button} onPress={() => Linking.openURL(`${mapUrl}${acommodation.lat},${acommodation.long}`)}>
-                        <Text style={styles.buttonText}>Ver no Mapa</Text>
-                        <FontAwesome name={"map-marker"} size={20} color="#fff" />
-                    </Pressable>
-                </View>
-            </ScrollView>
-        </SafeAreaView>
-    );
+        <View style={styles.infoCard}>
+          <View style={styles.infoRow}>
+            <FontAwesome name="money" size={20} color="#555" style={styles.infoIcon}/>
+            <View style={styles.infoTextContainer}>
+              <Text style={styles.infoLabel}>Valor</Text>
+              <Text style={styles.infoValue}>{acommodation.valor}</Text>
+            </View>
+          </View>
+          <View style={styles.infoRow}>
+            <FontAwesome name="envelope-o" size={20} color="#555" style={styles.infoIcon}/>
+            <View style={styles.infoTextContainer}>
+              <Text style={styles.infoLabel}>Email para contato</Text>
+              <Text style={styles.infoValue}>{acommodation.email}</Text>
+            </View>
+          </View>
+          <View style={styles.infoRow}>
+            <FontAwesome name="phone" size={20} color="#555" style={styles.infoIcon}/>
+            <View style={styles.infoTextContainer}>
+              <Text style={styles.infoLabel}>Telefone para contato</Text>
+              <Text style={styles.infoValue}>{acommodation.telefone}</Text>
+            </View>
+          </View>
+          <View style={styles.infoRow}>
+            <FontAwesome name="map-marker" size={20} color="#555" style={styles.infoIcon}/>
+            <View style={styles.infoTextContainer}>
+              <Text style={styles.infoLabel}>Localização</Text>
+              <Text style={styles.infoValue}>{acommodation.endereco}</Text>
+            </View>
+          </View>
+        </View>
+
+        <Pressable style={styles.button}
+                   onPress={() => Linking.openURL(`${mapUrl}${acommodation.lat},${acommodation.long}`)}>
+          <Text style={styles.buttonText}>Ver no Mapa</Text>
+          <FontAwesome name={"map-marker"} size={20} color="#fff"/>
+        </Pressable>
+      </View>
+    </ScrollView>
+  </SafeAreaView>);
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-    },
-    contentContainer: {
-        padding: 20,
-    },
-    backButton: {
-        alignSelf: 'flex-start',
-        padding: 10,
-        marginLeft: -10,
-    },
-    icon: {
-        width: 30,
-        height: 30
-    },
-    title: {
-        fontSize: 32,
-        fontWeight: "bold",
-        marginBottom: 20,
-        marginTop: 10,
-    },
-    image: {
-        width: '100%',
-        aspectRatio: 16 / 9,
-        borderRadius: 20,
-        marginBottom: 20,
-    },
-    infoCard: {
-        backgroundColor: '#f9f9f9',
-        borderRadius: 15,
-        padding: 20,
-        marginBottom: 20,
-        shadowColor: "#000",
-        shadowOffset: { width: 0, height: 2 },
-        shadowOpacity: 0.1,
-        shadowRadius: 3.84,
-        elevation: 5,
-    },
-    infoRow: {
-        flexDirection: 'row',
-        alignItems: 'center',
-        marginVertical: 10,
-    },
-    infoIcon: {
-        marginRight: 15,
-        width: 20,
-        textAlign: 'center',
-    },
-    infoTextContainer: {
-        flex: 1,
-    },
-    infoLabel: {
-        fontSize: 14,
-        color: '#666',
-    },
-    infoValue: {
-        fontSize: 16,
-        fontWeight: 'bold',
-    },
-    button: {
-        backgroundColor: "#000",
-        paddingVertical: 15,
-        borderRadius: 15,
-        flexDirection: 'row',
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
-    buttonText: {
-        fontSize: 18,
-        color: "#fff",
-        fontWeight: "bold",
-        marginRight: 12
-    },
+  container: {
+    flex: 1, backgroundColor: 'white',
+  }, carouselContainer: {
+    height: 250,
+  }, carouselImage: {
+    width: screenWidth, height: '100%', resizeMode: 'cover',
+  }, pagination: {
+    flexDirection: 'row', position: 'absolute', bottom: 15, alignSelf: 'center',
+  }, paginationDot: {
+    width: 8, height: 8, borderRadius: 4, backgroundColor: 'rgba(0, 0, 0, 0.4)', margin: 5,
+  }, paginationDotActive: {
+    backgroundColor: '#FFF',
+  }, backButton: {
+    position: 'absolute',
+    top: StatusBar.currentHeight ? StatusBar.currentHeight - 15 : 40,
+    left: 20,
+    zIndex: 1,
+    backgroundColor: 'rgba(0,0,0,0.4)',
+    padding: 8,
+    borderRadius: 20,
+  }, icon: {
+    width: 25, height: 25, tintColor: 'white'
+  }, contentContainer: {
+    padding: 20, backgroundColor: 'white'
+  }, title: {
+    fontSize: 32, fontWeight: "bold", marginBottom: 20,
+  }, infoCard: {
+    backgroundColor: '#f9f9f9',
+    borderRadius: 15,
+    padding: 20,
+    marginBottom: 20,
+    shadowColor: "#000",
+    shadowOffset: {width: 0, height: 2},
+    shadowOpacity: 0.1,
+    shadowRadius: 3.84,
+    elevation: 5,
+  }, infoRow: {
+    flexDirection: 'row', alignItems: 'center', marginVertical: 10,
+  }, infoIcon: {
+    marginRight: 15, width: 20, textAlign: 'center',
+  }, infoTextContainer: {
+    flex: 1,
+  }, infoLabel: {
+    fontSize: 14, color: '#666',
+  }, infoValue: {
+    fontSize: 16, fontWeight: 'bold',
+  }, button: {
+    backgroundColor: "#000",
+    paddingVertical: 15,
+    borderRadius: 15,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+  }, buttonText: {
+    fontSize: 18, color: "#fff", fontWeight: "bold", marginRight: 12
+  },
 });
--- app/accomodations/index.tsx
+++ app/accomodations/index.tsx
@@ -1,115 +1,98 @@
-import { SafeAreaView, StatusBar, Text, View, StyleSheet, Pressable, FlatList, Image } from "react-native";
-import { router } from "expo-router";
 import AsyncStorage from "@react-native-async-storage/async-storage";
+import { router } from "expo-router";
 import { useEffect, useState } from "react";
+import { FlatList, Image, Pressable, SafeAreaView, StatusBar, StyleSheet, Text, View } from "react-native";
+
 
 const ACCOMODATIONS_DATA_LINK = "https://raw.githubusercontent.com/estefanotuyama/jub-ufsc-data/refs/heads/main/accommodation.json";
 
 const saveAccomodations = async (accomodations) => {
-    const accomodationsJson = await JSON.stringify(accomodations);
-    await AsyncStorage.setItem("accomodations_cache", accomodationsJson);
+  const accomodationsJson = await JSON.stringify(accomodations);
+  await AsyncStorage.setItem("accomodations_cache", accomodationsJson);
 }
 
 const readAccomodations = async () => {
-    const accomodations = await AsyncStorage.getItem("accomodations_cache");
-    return accomodations != null ? JSON.parse(accomodations) : null;
+  const accomodations = await AsyncStorage.getItem("accomodations_cache");
+  return accomodations != null ? JSON.parse(accomodations) : null;
 }
 
 async function loadAccomodations() {
-    let data = await readAccomodations();
+  let data = await readAccomodations();
 
-    if (!data) {
-        const response = await fetch(ACCOMODATIONS_DATA_LINK);
-        data = await response.json();
-        await saveAccomodations(data);
-    }
-    return data;
+  if (!data) {
+    const response = await fetch(ACCOMODATIONS_DATA_LINK);
+    data = await response.json();
+    await saveAccomodations(data);
+  }
+  return data;
 }
 
-const AcommodationRender = ({ item }) => {
-    return (
-        <Pressable style={styles.pressable} onPress={() => router.push({ pathname: `/accomodations/${item.id}`, params: { ...item } })}>
-            <View>
-                <Text style={styles.mainText}>{item.nome}</Text>
-                <Text style={styles.text}>{item.bairro}</Text>
-            </View>
-            <Image style={styles.arrow} source={require("../../assets/images/arrow-right.png")} />
-        </Pressable>
-    )
+const AcommodationRender = ({item}) => {
+  return (<Pressable style={styles.pressable} onPress={() => {
+      const params = {...item, fotos: JSON.stringify(item.fotos)};
+      router.push({pathname: `/accomodations/${item.id}`, params: params});
+    }}>
+      <View>
+        <Text style={styles.mainText}>{item.nome}</Text>
+        <Text style={styles.text}>{item.bairro}</Text>
+      </View>
+      <Image style={styles.arrow} source={require("../../assets/images/arrow-right.png")}/>
+    </Pressable>)
 }
 
 export default function Accomodations() {
-    const [data, setData] = useState([]);
+  const [data, setData] = useState([]);
 
-        useEffect(() => {
-              const fetchData = async () => {
-                  const accomodations = await loadAccomodations();
-                  setData(accomodations);
-              };
-            fetchData();
-        }, []);
+  useEffect(() => {
+    const fetchData = async () => {
+      const accomodations = await loadAccomodations();
+      setData(accomodations);
+    };
+    fetchData();
+  }, []);
 
 
-    return (
-        <SafeAreaView style={styles.container}>
-            <StatusBar barStyle={"default"} />
-                <View style={styles.contentContainer}>
-                    <FlatList
-                        ListHeaderComponent={
-                          <Text style={styles.title}>Alojamentos</Text>
-                        }
-                        data={data}
-                        renderItem={({ item }) => <AcommodationRender item={item} />}
-                        keyExtractor={item => item.id}
-                    />
-                </View>
-        </SafeAreaView>
-    )
+  return (<SafeAreaView style={styles.container}>
+      <StatusBar barStyle={"default"}/>
+      <View style={styles.contentContainer}>
+        <FlatList
+          ListHeaderComponent={<Text style={styles.title}>Alojamentos</Text>}
+          data={data}
+          renderItem={({item}) => <AcommodationRender item={item}/>}
+          keyExtractor={item => item.id}
+        />
+      </View>
+    </SafeAreaView>)
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
+  container: {
+    flex: 1,
+  }, contentContainer: {
+    flex: 1, padding: 20
+  }, title: {
+    fontSize: 32, fontWeight: "bold", marginBottom: 20
+  }, pressable: {
+    alignItems: "center",
+    justifyContent: "space-between",
+    flexDirection: "row",
+    paddingHorizontal: 18,
+    height: 90,
+    backgroundColor: "white",
+    marginBottom: 20,
+    borderRadius: 25,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0, height: 3,
     },
-    contentContainer: {
-        flex: 1,
-        padding: 20
-    },
-    title: {
-        fontSize: 32,
-        fontWeight: "bold",
-        marginBottom: 20
-    },
-    pressable: {
-        alignItems: "center",
-        justifyContent: "space-between",
-        flexDirection: "row",
-        paddingHorizontal: 18,
-        height: 90,
-        backgroundColor: "white",
-        marginBottom: 20,
-        borderRadius: 25,
-        shadowColor: "#000",
-        shadowOffset: {
-            width: 0,
-            height: 3,
-        },
-        shadowOpacity: 0.1,
-        shadowRadius: 3.84,
-        elevation: 5,
-    },
-    arrow: {
-        height: 20,
-        width: 20,
-        marginLeft: 5
-    },
-    mainText: {
-        fontSize: 18,
-        fontWeight: "bold",
-    },
-    text: {
-        fontSize: 16,
-        color: '#666',
-        marginTop: 4,
-    },
+    shadowOpacity: 0.1,
+    shadowRadius: 3.84,
+    elevation: 5,
+  }, arrow: {
+    height: 20, width: 20, marginLeft: 5
+  }, mainText: {
+    fontSize: 18, fontWeight: "bold",
+  }, text: {
+    fontSize: 16, color: '#666', marginTop: 4,
+  },
 });
